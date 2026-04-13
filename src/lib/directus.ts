ceee5ba2f3a2f6642de0559366583154eb8901bf
Tobias <tobias.bp2004@gmail.com>
2026-04-13 14:28:08 -0300

    fix: build env

--- src/lib/directus.ts
+++ src/lib/directus.ts
@@ -1,10 +1,6 @@
 import { createDirectus, rest } from '@directus/sdk';
 import type { Schema } from '@/types';
 
-if (!process.env.NEXT_DIRECTUS_URL) {
-	throw new Error('NEXT_DIRECTUS_URL is not set');
-}
-
 const directus = createDirectus<Schema>(
 	process.env.NEXT_DIRECTUS_URL ?? '',
 ).with(rest());
